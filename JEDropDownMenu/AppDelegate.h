22332a0a04720be600a12f816c4dcb664b2dbc73
尹现伟 <ios@yinxianwei.cn>
2015-04-07 09:44:08 +0800

    Initial Commit

---
+++ JEDropDownMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JEDropDownMenu
+//
+//  Created by 尹现伟 on 15/4/7.
+//  Copyright (c) 2015年 上海美问信息技术有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JEDropDownMenu/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JEDropDownMenu
+//
+//  Created by 尹现伟 on 15/4/7.
+//  Copyright (c) 2015年 上海美问信息技术有限公司. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
